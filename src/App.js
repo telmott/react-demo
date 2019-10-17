d15c5c45bf18d31955f516e7baeae8aa5580fef5
Telmo Teixeira <telmo@widgilabs.com>
2019-10-18 00:45:40 +0100

    Final ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Text from './components/Text';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      day: true,
+      dayText: 'DAY',
+      nigthText: 'NIGHT'
+    }
+
+    this.getClassName = this.getClassName.bind(this);
+    this.changeText = this.changeText.bind(this);
+    this.getText = this.getText.bind(this);
+    this.toggleDay = this.toggleDay.bind(this);
+  }
+
+  toggleDay() {
+    this.setState(
+      { day: !this.state.day }
+    );
+  }
+
+  getClassName() {
+    if (this.state.day) {
+      return 'day';
+    }
+
+    return 'night';
+  }
+
+  changeText(e) {
+    var key = 'nigthText';
+
+    if (this.state.day) {
+      key = 'dayText';
+    }
+
+    this.setState(
+      {[key]: e.target.value}
+    );
+  }
+
+  getText() {
+    if (this.state.day) {
+      return this.state.dayText;
+    }
+
+    return this.state.nigthText;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className={'App-header ' + this.getClassName()}>
+          <button onClick={this.toggleDay}>Toggle</button>
+          <Text text={this.getText()} day={this.state.day} changeText={this.changeText}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Text.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Text(props) {
+    var textClass = 'text';
+
+    if (props.day) {
+        textClass += ' text__day day';
+    } else {
+        textClass += ' text__night night';
+    }
+
+    return (
+        <input type="text" className={textClass} value={props.text} onChange={props.changeText}/>
+    );
+}
+
+export default Text;
