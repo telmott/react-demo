18658c9433dd5b2383e497e561d1e3104f0520c6
Telmo Teixeira <telmo@widgilabs.com>
2019-10-18 13:27:23 +0100

    second component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,13 @@
 import React from 'react';
 import './App.css';
-import Text from './components/Text';
+// import Text from './components/Text';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      day: true,
-      dayText: 'DAY',
-      nigthText: 'NIGHT'
-    }
-
-    this.getClassName = this.getClassName.bind(this);
-    this.changeText = this.changeText.bind(this);
-    this.getText = this.getText.bind(this);
-    this.toggleDay = this.toggleDay.bind(this);
-  }
-
-  toggleDay() {
-    this.setState(
-      { day: !this.state.day }
-    );
-  }
-
-  getClassName() {
-    if (this.state.day) {
-      return 'day';
-    }
-
-    return 'night';
-  }
-
-  changeText(e) {
-    var key = 'nigthText';
-
-    if (this.state.day) {
-      key = 'dayText';
-    }
-
-    this.setState(
-      {[key]: e.target.value}
-    );
-  }
-
-  getText() {
-    if (this.state.day) {
-      return this.state.dayText;
-    }
-
-    return this.state.nigthText;
-  }
-
   render() {
     return (
       <div className="App">
-        <header className={'App-header ' + this.getClassName()}>
-          <button onClick={this.toggleDay}>Toggle</button>
-          <Text text={this.getText()} day={this.state.day} changeText={this.changeText}/>
+        <header className="App-header">
+          <h1>Vamos lá meter um componente aqui!</h1>
         </header>
       </div>
     );
@@ -64,3 +15,5 @@ class App extends React.Component {
 }
 
 export default App;
+
+// <Text text={'isto é o componente Text'} />
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
 function Text(props) {
-    var textClass = 'text';
-
-    if (props.day) {
-        textClass += ' text__day day';
-    } else {
-        textClass += ' text__night night';
-    }
 
     return (
-        <input type="text" className={textClass} value={props.text} onChange={props.changeText}/>
+        <h1>{props.text}</h1>
     );
 }
 
