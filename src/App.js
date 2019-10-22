2bf96d72ea0185136a80ee8314ca81c4063dcefb
Marco Peralta <marco@widgilabs.com>
2019-10-22 12:23:37 +0100

    theme provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import './App.css';
 
+const theme = {
+  primary: 'red',
+  secondary: 'green',
+  alert: 'yellow',
+  font: 'sans-serif'
+}
+
 const Button = styled.button`
-  font-family: sans-serif;
+  font-family: ${props => props.theme.font};
   font-size: 1.3rem;
   border: none;
   border-radius: 5px;
   padding: 7px 10px;
-  background: ${props => props.primary ? 'red' : 'green' };
+  /* background: ${props => props.primary ? 'red' : 'green' }; */
+  background: ${props => props.color && props.theme[props.color] || props.theme.primary};
   color: #fff;
 
   &:hover {
@@ -19,15 +27,17 @@ const Button = styled.button`
 class App extends React.Component {
   render() {
     return (
+      <ThemeProvider theme={theme}>
         <div className="App">
           <h1>styled components</h1>
           <form action="">
             <input type="text"/>
             <button>create</button>
             <Button>Create</Button>
-            <Button primary>Create</Button>
+            <Button color="alert">Create</Button>
           </form>
         </div>
+      </ThemeProvider>
     );
   }
 }
